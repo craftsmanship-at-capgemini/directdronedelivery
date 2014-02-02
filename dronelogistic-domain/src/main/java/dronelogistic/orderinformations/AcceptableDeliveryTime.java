25e6121780637452056ff7b216c1da7b0007aa8b
Michal Michaluk <michal.michaluk@capgemini.com>
2014-02-02 23:50:33 +0100

    AcceptableDeliveryTime and DeliveryTimeAcceptanceStrategy done

--- dronelogistic-domain/src/main/java/dronelogistic/orderinformations/AcceptableDeliveryTime.java
+++ dronelogistic-domain/src/main/java/dronelogistic/orderinformations/AcceptableDeliveryTime.java
@@ -1,10 +1,32 @@
 package dronelogistic.orderinformations;
 
+import java.util.BitSet;
+
 import lombok.EqualsAndHashCode;
 import lombok.ToString;
+
+import org.joda.time.DateTime;
 
 @EqualsAndHashCode
 @ToString
 public class AcceptableDeliveryTime {
+    
+    BitSet hours;
+    BitSet halves;
+    
+    AcceptableDeliveryTime() {
+        hours = new BitSet(24);
+        halves = new BitSet(24);
+    }
+    
+    public boolean isInAcceptableTime(DateTime time) {
+        int minute = time.getMinuteOfHour();
+        int hour = time.getHourOfDay();
+        if (minute < 30) {
+            return hours.get(hour);
+        } else {
+            return halves.get(hour);
+        }
+    }
     
 }
